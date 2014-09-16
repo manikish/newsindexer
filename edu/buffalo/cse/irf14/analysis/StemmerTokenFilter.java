32ef21b70e124049f610705fddfd1acbaec3f8c3
Narasimha Sumanth Yajamanyam <sumanth_yn@yahoo.com>
2014-09-16 00:27:10 -0400

    mani pulka

---
+++ edu/buffalo/cse/irf14/analysis/StemmerTokenFilter.java
@@ -0,0 +1,29 @@
+package edu.buffalo.cse.irf14.analysis;
+
+public class StemmerTokenFilter extends TokenFilter {
+
+	@Override
+	public boolean increment() throws TokenizerException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setNextFilter(TokenFilter nextFilter) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public TokenFilter getNextFilter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void perform() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
